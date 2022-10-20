2865b72935c580b431151fdb4b846422fae982c6
david.breau <davidbreau@live.fr>
2022-10-20 10:57:40 +0200

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by apprenant49 on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
